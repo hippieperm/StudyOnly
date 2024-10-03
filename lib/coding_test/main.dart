4a48623d87cd9ceb79e58e0c5e973a8fae0b2ebb
hippieperm <ghl0617@gmail.com>
2024-10-03 20:57:53 +0900

    Coding Test 정리

---
+++ lib/coding_test/main.dart
@@ -0,0 +1,12 @@
+import 'solution.dart';
+
+void main() {
+  print(Solution().solution('aaaa', 'bbbb'));
+
+  print(Solution().solution1(['a', 'b', 'c', 'd']));
+
+  print(Solution().solution2('str', 3));
+
+  print(Solution().solution3(7));
+  print(Solution().solution3(10));
+}
