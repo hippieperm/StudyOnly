cfef225781f2068c1f95d5a4e72dd45b01e1da90
hippieperm <leess0133@gmail.com>
2024-11-07 23:03:16 +0900

    1107_웹툰 연습

---
+++ lib/webtoon_app_v2/v3_9/main.dart
@@ -0,0 +1,15 @@
+import 'package:first_pj/ui_clone/main.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
