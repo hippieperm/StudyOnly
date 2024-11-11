3c539860b3ee498d576d88d5e405558a59953a32
hippieperm <leess0133@gmail.com>
2024-11-11 23:04:24 +0900

    11.11_웹툰 연습

---
+++ lib/v5_webtoon/screens/v5_home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:first_pj/v5_webtoon/models/v5_webtoon_model.dart';
+import 'package:first_pj/v5_webtoon/services/v5_api_service.dart';
+import 'package:flutter/material.dart';
+
+class V5HomeScreen extends StatelessWidget {
+  V5HomeScreen({super.key});
+  Future<List<V5WebtoonModel>> webtoon = V5ApiService.getTodayToons();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff212528),
+      appBar: AppBar(
+        backgroundColor: const Color(0xff212528),
+        title: Text(
+          "Today's Toon",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w400,
+            color: Colors.white.withOpacity(0.8),
+          ),
+        ),
+      ),
+      body: FutureBuilder(
+        future: webtoon,
+        builder: (context, snapshot) {},
+      ),
+    );
+  }
+}
