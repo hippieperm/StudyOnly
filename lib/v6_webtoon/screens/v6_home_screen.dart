15a9098d659080206cc891df86a755c63f563eab
hippieperm <leess0133@gmail.com>
2024-11-13 23:01:55 +0900

    11.13_웹툰 연습

--- lib/v6_webtoon/screens/v6_home_screen.dart
+++ lib/v6_webtoon/screens/v6_home_screen.dart
@@ -3,7 +3,7 @@ import 'package:first_pj/v6_webtoon/services/v6_api_service.dart';
 import 'package:flutter/material.dart';
 
 class V6HomeScreeen extends StatelessWidget {
-   V6HomeScreeen({super.key});
+  V6HomeScreeen({super.key});
 
   Future<List<V6WebtoonModel>> webtoons = V6ApiService.getToons();
 
@@ -24,7 +24,19 @@ class V6HomeScreeen extends StatelessWidget {
       body: FutureBuilder(
         future: webtoons,
         builder: (context, snapshot) {
-
+          if (snapshot.hasData) {
+            return ListView.separated(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                print(index);
+                return null;
+              },
+              separatorBuilder: (context, index) => const SizedBox(height: 24),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         },
       ),
     );
