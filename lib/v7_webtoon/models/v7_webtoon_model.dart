a1aa32f826a728a2f3f28ad5335c5b69beb9ed9a
hippieperm <leess0133@gmail.com>
2024-11-14 23:20:18 +0900

    11.14_웹툰 연습__5

--- lib/v7_webtoon/models/v7_webtoon_model.dart
+++ lib/v7_webtoon/models/v7_webtoon_model.dart
@@ -1,1 +1,19 @@
-class V7WebtoonModel {}
+class V7WebtoonModel {
+  final String id;
+  final String title;
+  final String thumb;
+
+  V7WebtoonModel({
+    required this.id,
+    required this.title,
+    required this.thumb,
+  });
+
+  factory V7WebtoonModel.fromJson(Map<String, dynamic> json) {
+    return V7WebtoonModel(
+      id: json['id'],
+      title: json['title'],
+      thumb: json['thumb'],
+    );
+  }
+}
