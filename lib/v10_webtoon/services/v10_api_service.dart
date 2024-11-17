2a04fdf5f60f25145e321d17a48632a6b100d18c
hippieperm <leess0133@gmail.com>
2024-11-17 22:51:08 +0900

    11.17_웹툰 연습_6

--- lib/v10_webtoon/services/v10_api_service.dart
+++ lib/v10_webtoon/services/v10_api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:first_pj/v10_webtoon/models/v10_webtoon_model.dart';
 import 'package:http/http.dart';
 
 class V10ApiService {
@@ -7,16 +8,19 @@ class V10ApiService {
       'https://webtoon-crawler.nomadcoders.workers.dev';
   static const String today = 'today';
 
-  static Future<void> getTodayToons() async {
+  static Future<List<V10WebtoonModel>> getTodayToons() async {
     final url = Uri.parse('$baseUrl/$today');
     final response = await get(url);
+    List<V10WebtoonModel> webtoonInstance = [];
 
     if (response.statusCode == 200) {
       List<dynamic> jsonBody = jsonDecode(response.body);
 
       for (var json in jsonBody) {
-        
+        webtoonInstance.add(V10WebtoonModel.fromJson(json));
       }
+      return webtoonInstance;
     }
+    throw Error();
   }
 }
