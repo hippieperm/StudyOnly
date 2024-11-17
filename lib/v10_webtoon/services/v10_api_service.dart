1a5d38f17473e80f54470878bc7ed0df94fa24a1
hippieperm <leess0133@gmail.com>
2024-11-17 22:48:18 +0900

    11.17_웹툰 연습_4

--- lib/v10_webtoon/services/v10_api_service.dart
+++ lib/v10_webtoon/services/v10_api_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart';
 
 class V10ApiService {
@@ -10,7 +12,11 @@ class V10ApiService {
     final response = await get(url);
 
     if (response.statusCode == 200) {
-      
+      List<dynamic> jsonBody = jsonDecode(response.body);
+
+      for (var json in jsonBody) {
+        
+      }
     }
   }
 }
