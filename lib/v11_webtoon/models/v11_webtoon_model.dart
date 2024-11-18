3f96b0a58d75e09179df2a85573b9bf9731b0da1
hippieperm <leess0133@gmail.com>
2024-11-18 20:34:47 +0900

    11.18_웹툰 연습_6

--- lib/v11_webtoon/models/v11_webtoon_model.dart
+++ lib/v11_webtoon/models/v11_webtoon_model.dart
@@ -2,4 +2,18 @@ class V11WebtoonModel {
   final String id;
   final String title;
   final String thumb;
+
+  V11WebtoonModel({
+    required this.id,
+    required this.title,
+    required this.thumb,
+  });
+
+  factory V11WebtoonModel.fromJson(Map<String, dynamic> json) {
+    return V11WebtoonModel(
+      id: json['id'],
+      title: json['title'],
+      thumb: json['thumb'],
+    );
+  }
 }
