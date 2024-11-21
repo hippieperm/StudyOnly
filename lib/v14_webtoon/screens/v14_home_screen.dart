2e36cb6d55a4bb9c6c5fbf85ee6da93508b75a47
hippieperm <leess0133@gmail.com>
2024-11-21 23:13:06 +0900

    11.21_웹툰 연습_3

--- lib/v14_webtoon/screens/v14_home_screen.dart
+++ lib/v14_webtoon/screens/v14_home_screen.dart
@@ -5,6 +5,19 @@ class V14HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: const Color(0xff212528),
+      appBar: AppBar(
+        backgroundColor: const Color(0xff212528),
+        title: Text(
+          'TOOOOON!!!!',
+          style: TextStyle(
+            color: Colors.white.withOpacity(0.8),
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
