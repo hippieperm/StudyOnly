c2e8aa6de35696d40415229ae01214e8b9ef4761
hippieperm <leess0133@gmail.com>
2024-11-21 23:10:05 +0900

    11.21_웹툰 연습_2

---
+++ lib/v14_webtoon/screens/v14_home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class V14HomeScreen extends StatelessWidget {
+  const V14HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
