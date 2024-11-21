5584ca4d369cc906091bb208b79de9704993e4c9
hippieperm <leess0133@gmail.com>
2024-11-21 23:15:35 +0900

    11.21_웹툰 연습_4

---
+++ lib/v14_webtoon/services/v14_api_service.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+class V14ApiService {
+  static const String baseUrl =
+      'https://webtoon-crawler.nomadcoders.workers.dev';
+  static const String today = 'today';
+
+  static Future<void> getToons() async {
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await get(url);
+
+    if (response == 200) {
+      List<dynamic> jsonBody = jsonDecode(response.body);
+      
+    }
+  }
+}
