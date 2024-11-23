a58f33dbfab541a25f598ee7af9412a34f4876cf
hippieperm <leess0133@gmail.com>
2024-11-23 22:35:07 +0900

    11.23_웹툰 연습_2

---
+++ lib/v16_webtoon/screen/v16_home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class V16HomeScreen extends StatelessWidget {
+  const V16HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff212528),
+      appBar: AppBar(
+        backgroundColor: const Color(0xff212528),
+        title: Text(
+          'TOooOOoooN!',
+          style: TextStyle(
+            color: Colors.white.withOpacity(0.8),
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
