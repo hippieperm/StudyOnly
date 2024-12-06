0867d54c58e592f65963d7ea7acbf9b507b95831
hippieperm <leess0133@gmail.com>
2024-12-06 23:33:52 +0900

    12.06_webtoon_4

--- lib/v19_webtoon/main.dart
+++ lib/v19_webtoon/main.dart
@@ -1,8 +1,10 @@
 import 'package:first_pj/v19_webtoon/screens/v19_home_screen.dart';
+import 'package:first_pj/v19_webtoon/services/v19_api_service.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const App());
+  V19ApiService.getToons();
 }
 
 class App extends StatelessWidget {
