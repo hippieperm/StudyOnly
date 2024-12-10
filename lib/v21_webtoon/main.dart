80328d1f6a14cf65a0344e33d56f34f3e3b322f1
hippieperm <leess0133@gmail.com>
2024-12-10 22:37:53 +0900

    12.10_webtoon_5

---
+++ lib/v21_webtoon/main.dart
@@ -0,0 +1,18 @@
+import 'package:first_pj/v20_webtoon/main.dart';
+import 'package:first_pj/v21_webtoon/screen/v21_home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: V21HomeScreen(),
+    );
+  }
+}
