5867c90149b5d5f8418f8c5f86627f5bedeb42e1
hippieperm <leess0133@gmail.com>
2024-12-11 23:27:39 +0900

    12.11_webtoon_4

--- lib/v22_webtoon/model/v22_webtoon_model.dart
+++ lib/v22_webtoon/model/v22_webtoon_model.dart
@@ -2,4 +2,18 @@ class V22WebtoonModel {
   final String id;
   final String title;
   final String thumb;
+
+  V22WebtoonModel({
+    required this.id,
+    required this.title,
+    required this.thumb,
+  });
+
+  factory V22WebtoonModel.fromJson(Map<String, dynamic> json) {
+    return V22WebtoonModel(
+      id: json['id'],
+      title: json['title'],
+      thumb: json['thumb'],
+    );
+  }
 }
