8c57010db52ce9c11a9a771d7df47cb0018c50e9
hippieperm <leess0133@gmail.com>
2024-12-11 23:30:12 +0900

    12.11_webtoon_6

--- lib/v22_webtoon/screen/v22_home_screen.dart
+++ lib/v22_webtoon/screen/v22_home_screen.dart
@@ -22,6 +22,10 @@ class V22HomeScreen extends StatelessWidget {
           ),
         ),
       ),
+      body: FutureBuilder(
+        future: future,
+        builder: builder,
+      ),
     );
   }
 }
