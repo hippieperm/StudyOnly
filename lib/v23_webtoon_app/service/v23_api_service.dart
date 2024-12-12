fc01bf2bc4a4a02c0b1c8d3825c5db1a4d6750c9
hippieperm <leess0133@gmail.com>
2024-12-12 22:55:53 +0900

    12.12_webtoon_temp_9

--- lib/v23_webtoon_app/service/v23_api_service.dart
+++ lib/v23_webtoon_app/service/v23_api_service.dart
@@ -1,21 +1,25 @@
 import 'dart:convert';
 
+import 'package:first_pj/v23_webtoon_app/model/v23_webtoon_model.dart';
 import 'package:http/http.dart';
 
 class V23ApiService {
   static const String baseUrl =
       'https://webtoon-crawler.nomadcoders.workers.dev';
   static const String today = 'today';
-  static Future<void> getToons() async {
+  static Future<List<V23WebtoonModel>> getToons() async {
     final url = Uri.parse('$baseUrl/$today');
     final response = await get(url);
 
     if (response.statusCode == 200) {
       var jsonBody = jsonDecode(response.body);
+      List<V23WebtoonModel> webtoonInstances = [];
 
       for (var json in jsonBody) {
-        
+        webtoonInstances.add(V23WebtoonModel.fromJson(json));
       }
+      return webtoonInstances;
     }
+    throw Error();
   }
 }
