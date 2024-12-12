7e1af21e699642fd396bb42cbef7c81e205beca6
hippieperm <leess0133@gmail.com>
2024-12-12 22:46:47 +0900

    12.12_webtoon_temp_4

---
+++ lib/v23_webtoon_app/screen/v23_home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class V23HomeScreen extends StatelessWidget {
+  const V23HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
