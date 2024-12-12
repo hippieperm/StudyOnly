83c3b04c5f11e1e48feabc06a2bdc922d7be73a8
hippieperm <leess0133@gmail.com>
2024-12-12 22:50:50 +0900

    12.12_webtoon_temp_6

---
+++ lib/v23_webtoon_app/service/v23_api_service.dart
@@ -0,0 +1,6 @@
+class V23ApiService {
+  static const String baseUrl =
+      'https://webtoon-crawler.nomadcoders.workers.dev';
+  static const String today = 'today';
+  static void getToons() {}
+}
