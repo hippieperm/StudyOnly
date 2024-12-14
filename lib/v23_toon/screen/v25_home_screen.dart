81e5b4c1326e4102f3b4b80613dcf760010fa2cb
hippieperm <leess0133@gmail.com>
2024-12-14 23:12:32 +0900

    12.14_webtoon_TEMP_5

--- lib/v23_toon/screen/v25_home_screen.dart
+++ lib/v23_toon/screen/v25_home_screen.dart
@@ -5,10 +5,19 @@ class V25HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor: Color(0xff212528),
-
-      
+    return Scaffold(
+      backgroundColor: const Color(0xff212528),
+      appBar: AppBar(
+        backgroundColor: const Color(0xff212528),
+        title: const Text(
+          'TOON!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
