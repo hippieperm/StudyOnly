16220d9a39a769da7665b8505f3b572aadd08f7c
hippieperm <leess0133@gmail.com>
2024-12-15 23:19:25 +0900

    12.15_TOON_TEMP_02

---
+++ lib/v26_toon/main.dart
@@ -0,0 +1,17 @@
+import 'package:first_pj/v26_toon/screen/v26_home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: V26HomeScreen(),
+    );
+  }
+}
