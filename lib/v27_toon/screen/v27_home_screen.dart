dd2110bdf42861efa3bea22f7fd4e1e19464ba65
hippieperm <leess0133@gmail.com>
2024-12-17 23:12:36 +0900

    12.17_TOON_TEMP_03

---
+++ lib/v27_toon/screen/v27_home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class V27HomeScreen extends StatelessWidget {
+  const V27HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
