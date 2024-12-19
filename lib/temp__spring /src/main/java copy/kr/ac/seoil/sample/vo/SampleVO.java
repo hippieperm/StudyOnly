d9e1c1a2534b6dbb9a3b194bb627cb3dbff915e5
hippieperm <leess0133@gmail.com>
2024-12-19 22:55:36 +0900

    12.19__Spring__07

---
+++ lib/temp__spring /src/main/java copy/kr/ac/seoil/sample/vo/SampleVO.java
@@ -0,0 +1,14 @@
+package kr.ac.seoil.sample.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SampleVO {
+	private Integer mno;
+	private String firstName;
+	private String lastName;
+}
