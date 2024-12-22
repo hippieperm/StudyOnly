9a8aceb99419f149a4841cd646e9c5a407d93207
hippieperm <leess0133@gmail.com>
2024-12-22 23:10:41 +0900

    12.22_TOON_01

---
+++ lib/v29_toon/screen/v29_home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class V29HomeScreen extends StatelessWidget {
+  const V29HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
