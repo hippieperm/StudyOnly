03e76adfe2f31d900024fe8e7a6a284c90198a88
hippieperm <leess0133@gmail.com>
2024-12-22 23:15:55 +0900

    12.22_TOON_03

--- lib/v29_toon/screen/v29_home_screen.dart
+++ lib/v29_toon/screen/v29_home_screen.dart
@@ -5,6 +5,19 @@ class V29HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: const Color(0xff212528),
+      appBar: AppBar(
+        backgroundColor: const Color(0xff212528),
+        title: Text(
+          'TOON!',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white.withOpacity(0.8),
+          ),
+        ),
+      ),
+    );
   }
 }
