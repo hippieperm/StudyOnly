ae127e38225f3daa6bccbeb0d5f672424eb7db3d
hippieperm <leess0133@gmail.com>
2024-12-22 23:18:26 +0900

    12.22_TOON_05

--- lib/v29_toon/service/v29_api_service.dart
+++ lib/v29_toon/service/v29_api_service.dart
@@ -1,5 +1,22 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
 class V29ApiService {
   static const String baseUrl =
       'https://webtoon-crawler.nomadcoders.workers.dev';
   static const String today = 'today';
+
+  static Future<void> getToon() async {
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await get(url);
+
+    if (response.statusCode == 200) {
+      var jsonBody = jsonDecode(response.body);
+
+      for (var json in jsonBody) {
+        
+      }
+    }
+  }
 }
