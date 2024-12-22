f22f3b73c4892b53108148efdd98d7a7bb3fcd0d
hippieperm <leess0133@gmail.com>
2024-12-22 23:21:14 +0900

    12.22_TOON_08

--- lib/v29_toon/screen/v29_home_screen.dart
+++ lib/v29_toon/screen/v29_home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class V29HomeScreen extends StatelessWidget {
-  const V29HomeScreen({super.key});
+   V29HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
