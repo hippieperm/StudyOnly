0d3154b2b500f3959e9bc7ecad28533030db7f58
hippieperm <leess0133@gmail.com>
2024-12-24 23:41:10 +0900

    12.24_TOON_02

--- lib/temp/v29_toon/main.dart
+++ lib/temp/v29_toon/main.dart
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: V29HomeScreen(),
+      home: V30HomeScreen(),
     );
   }
 }
