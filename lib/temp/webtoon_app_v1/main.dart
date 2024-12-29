896991c72b8946137e87a4ba563b897d6cdf1e45
hippieperm <leess0133@gmail.com>
2024-12-29 22:33:13 +0900

    12.29_TOON_TEMP_10

---
+++ lib/temp/webtoon_app_v1/main.dart
@@ -0,0 +1,16 @@
+import 'package:first_pj/webtoon_app_v1/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
